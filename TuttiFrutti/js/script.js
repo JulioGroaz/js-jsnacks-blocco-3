e1477f914e55a205d34bcd6cdecd0d50ee2b2c23
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-14 14:14:58 +0200

    Ho creato l'array e lho riempito con numeri casuali.

--- TuttiFrutti/js/script.js
+++ TuttiFrutti/js/script.js
@@ -1,2 +1,7 @@
 const elementArray = Number.parseInt(prompt("quanti elementi vuoi che siano presenti nell'Array?"),10);
 
+const array = [];
+
+for(let i =0; i<elementArray; i++){
+    array[i]= Math.floor(Math.random() *100)+1;
+}
