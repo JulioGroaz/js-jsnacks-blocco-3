f48d7ab3190535044cd0e490f98e6922be72a0c8
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-23 12:37:43 +0200

    Array con persone fatto

---
+++ js-snack-object-2/script.js
@@ -0,0 +1,61 @@
+
+// Crea un array di oggetti che rappresentano delle persone, 10.
+// Ogni persona ha un nome, un cognome e un'età.
+
+// Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+const personeArray = [
+    {
+        nome: 'Gianni' ,
+        cognome: 'Giannari',
+        età: 55,
+    },
+    {
+        nome: 'Beppa',
+        cognome: 'Beppina',
+        età: 10,
+    },
+    {
+        nome: 'Zeus',
+        cognome: '?',
+        età: 293894382928120,
+    },
+    {
+        nome: 'Cristoph',
+        cognome: 'America',
+        età: 70,
+    },
+    {
+        nome: 'Franz',
+        cognome: 'Znarf',
+        età: 30,
+    },
+    {
+        nome: 'Gino',
+        cognome: 'Onig',
+        età: 2,
+    },
+    {
+        nome: 'Dante',
+        cognome: 'Alighieroz',
+        età: 18,
+    },
+    {
+        nome: 'Lorem',
+        cognome: 'Plutino',
+        età: 34,
+    },
+    {
+        nome: 'Simon',
+        cognome: 'Saponetta',
+        età: 5,
+    },
+    {
+        nome: 'Lolla',
+        cognome: 'Lulli',
+        età: 3,
+    },
+];
+
+
+
