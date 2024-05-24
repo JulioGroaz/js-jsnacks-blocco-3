4ea8351a4b15cb3a13c7253f31aff0bb31894c5f
JulioGroaz <julio.groaz2020@gmail.com>
2024-05-24 11:21:50 +0200

    array zucchine creato

---
+++ js-snack-zucchine/script.js
@@ -0,0 +1,62 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+// Dividi in due array separati le zucchine che misurano meno o più di 8cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+const zucchine = [
+    {
+        varietà: 'tipo1',
+        peso: 5,
+        lunghezza: 15,
+    },
+
+    {
+        varietà: 'tipo2',
+        peso: 3,
+        lunghezza: 8,
+    },
+    {
+        varietà: 'tipo3',
+        peso: 7,
+        lunghezza: 4,
+    },
+    {
+        varietà: 'tipo4',
+        peso: 9,
+        lunghezza: 45,
+    },
+    {
+        varietà:'tipo5' ,
+        peso: 10,
+        lunghezza: 17,
+    },
+    {
+        varietà: 'tipo6',
+        peso: 22,
+        lunghezza: 20,
+    },
+    {
+        varietà: 'tipo7',
+        peso: 54,
+        lunghezza: 50,
+    },
+    {
+        varietà: 'tipo8',
+        peso: 1,
+        lunghezza: 1,
+    },
+    {
+        varietà: 'tipo9',
+        peso: 4,
+        lunghezza: 6,
+    },
+    {
+        varietà: 'tipo10',
+        peso: 6,
+        lunghezza: 9,
+    },
+
+
+];
+
+
